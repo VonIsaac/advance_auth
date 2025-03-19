138ea119e3a78baa8ec1d1dae975a8d9b8c8a617
VonIsaac <119748258+VonIsaac@users.noreply.github.com>
2025-03-19 13:55:56 +0800

    Added LandingPage and store index; modified authentication and routing logic

--- src/utils/ProtectedRoutes.jsx
+++ src/utils/ProtectedRoutes.jsx
@@ -2,26 +2,19 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../components/store/AuthProvider";
 
-const ProtectedRoutes = ({ requiredRole, children }) => {
-    const {user} =  useContext(AuthContext)
+const ProtectedRoutes = ({ roles, children}) => {
+    const { user } = useContext(AuthContext);
 
-    if(!user){
-        return <Navigate to='/' />
-    }
-
-
-    if(user.role && !requiredRole ){
-        return  <Navigate to='/' />
-    }
-
-    return children 
-
-    // Check if user has the required role
-    /*if (requiredRole && user.role !== requiredRole) {
+    if (!user) {
         return <Navigate to="/" replace />;
-    };
+    }
 
-    return user?  <Outlet />: <Navigate to='/' />*/
+    if (roles && !roles.includes(user.role)) {
+        return <Navigate to="/page" replace />; // Redirect unauthorized users to home
+    }
+
+    return children; // Allows access to the route
 };
 
 export default ProtectedRoutes;
+
